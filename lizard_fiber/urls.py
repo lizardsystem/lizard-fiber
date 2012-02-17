cce964df7a3f8fb8a7a70d35b710c4e0e197e535
Reinout van Rees <reinout@vanrees.org>
2012-02-17 12:26:35 +0100

    Added fiber's required urls to our own to make it easier to setup.

--- lizard_fiber/urls.py
+++ lizard_fiber/urls.py
@@ -8,11 +8,19 @@ from lizard_ui.urls import debugmode_urlpatterns
 
 admin.autodiscover()
 
+
 urlpatterns = patterns(
     '',
-    url(r'^admin/', include(admin.site.urls)),
     # url(r'^something/',
     #     direct.import.views.some_method,
     #     name="name_it"),
+    # Only needed when developing on lizard_fiber.
+    url(r'^admin/', include(admin.site.urls)),
+    # Fiber urls. This means lizard_fiber's urls needs to be mounted as ''.
+    (r'^api/v1/', include('fiber.api.urls')),
+    (r'^admin/fiber/', include('fiber.admin_urls')),
+    (r'^jsi18n/$', 'django.views.i18n.javascript_catalog', {'packages': ('fiber',),}),
+    # Fiber catch-all. Needs to be at the end.
+    (r'', 'fiber.views.page'),
     )
 urlpatterns += debugmode_urlpatterns()
